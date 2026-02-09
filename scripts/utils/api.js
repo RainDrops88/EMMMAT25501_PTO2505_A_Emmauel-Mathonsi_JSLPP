9b3c2103bb1c96675cf554c437b9f911e252b58e
Emmanuel <07942jerry@gmail.com>
2026-02-09 22:31:07 +0200

    setting data to local storage

--- scripts/utils/api.js
+++ scripts/utils/api.js
@@ -2,7 +2,8 @@ export async function fetchData() {
   try {
     const response = await fetch('https://jsl-kanban-api.vercel.app');
     const data = await response.json();
-   return data;
+    localStorage.setItem("tasks", JSON.stringify(data));
+    return data;
   } catch (error) {
     console.error('Error fetching data:', error);
     return [];
